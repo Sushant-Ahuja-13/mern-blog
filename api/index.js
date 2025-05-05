ca96d2c3cdde432adb4d4c2aec1ad3a3e2786cb4
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-05-05 14:31:09 +0530

    create middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -17,8 +17,18 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
     res.json({ message: 'Api working!!' });
 })
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
+})
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
 })
