c4b21148a182f860ded6537260682c712d872d07
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-05-05 15:49:47 +0530

    create signup page ui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,14 @@
-import flowbiteReact from "flowbite-react/plugin/tailwindcss";
+import flowbitePlugin from "flowbite/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    ".flowbite-react/class-list.json"
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}", 
   ],
   theme: {
     extend: {},
   },
-  plugins: [flowbiteReact],
-}
+  plugins: [flowbitePlugin], 
+};
