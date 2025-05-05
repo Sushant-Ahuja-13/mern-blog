036552cbffe8e5776e941b8fc6cfa121c09af308
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-05-05 11:54:33 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDB Connected');
+}).catch((err) => {
+    console.log(err);
+})
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
