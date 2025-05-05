06a9eb37be3eef1b747668b0cf01b6948d1f9a0a
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-05-05 13:44:04 +0530

    create a test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('MongoDB Connected');
@@ -11,6 +12,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 })
+app.get('/test', (req, res) => {
+    res.json({ message: 'Api working!!' });
+})
+app.use('/api/user', userRoutes);
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 })
