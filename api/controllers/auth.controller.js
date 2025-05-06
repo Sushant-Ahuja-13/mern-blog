7b63e4ecadf7bce39517f8b00d51cd445d5f74e7
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-05-06 10:53:23 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 export const signup = async (req, res, next) => {
@@ -18,5 +19,27 @@ export const signup = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
-
+}
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+    }
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'Invalid credentials'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, 'Invalid credentials'));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
+    } catch (err) {
+        next(err);
+    }
 }
